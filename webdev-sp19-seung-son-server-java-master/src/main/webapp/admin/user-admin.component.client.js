4e2c6de192e0294dc40ef914b3040de61088301d
Jae Son <sjson421@gmail.com>
2019-01-22 14:25:06 -0500

    small progress on both component and service

--- webdev-sp19-seung-son-server-java-master/src/main/webapp/admin/user-admin.component.client.js
+++ webdev-sp19-seung-son-server-java-master/src/main/webapp/admin/user-admin.component.client.js
@@ -1,41 +1,73 @@
 (function() {
-	var $usernameFld, $passwordFld;
-	var $removeBtn, $editBtn, $createBtn;
-	var $firstNameFld, $lastNameFld;
+	var $usernameFld, $passwordFld, $firstNameFld, $lastNameFld, $roleFld;
+	var $removeBtn, $editBtn, $createBtn, $updateBtn;
 	var $userRowTemplate, $tbody;
 	var userService = new AdminUserServiceClient();
 	$(main);
 
 	function main() {
 		$usernameFld = $("#usernameFld");
+		$passwordFld = $("#passwordFld");
 		$firstNameFld = $("#firstNameFld");
+		$lastNameFld = $("#lastNameFld");
+		$roleFld = $("#roleFld")
+		$removeBtn = $("#wbdv-remove");
+		$editBtn = $("#wbdv-edit");
+		$updateBtn = $(".wbdv-update");
+		$createBtn = $(".wbdv-create");
 		$userRowTemplate = $(".wbdv-template");
 		$tbody = $("tbody");
 
-		$usernameFld.val("alice");
-
-		userService.findAllUsers().then(renderUsers);
+		findAllUsers();
+		
+		$removeBtn.click(deleteUser);
+		$editBtn.click(findUserById);
+		$updateBtn.click(updateUser);
+		$createBtn.click(createUser);
 	}
 	function createUser() {
+		const user = {
+				username: $usernameFld,
+				password: $passwordFld,
+				firstName: $firstNameFld,
+				lastName: $lastNameFld,
+				role: $roleFld
+		}
+		userService.createUser(user);
+		findAllUsers();
 	}
 	function findAllUsers() {
+		//TODO: find clicked id
+		return userService.findAllUsers().then(renderUsers);
 	}
 	function findUserById() {
-	}
-	function deleteUser() {
-	}
-	function selectUser() {
+		const id = $(this).closest('table').attr('id')
+		return userService.findUserById(id)
 	}
 	function updateUser() {
+		//TODO: find clicked id
+		return userService.updateUser($)
+	}
+	function deleteUser() {
+		//TODO: find clicked id
+		return userService.deleteUser($)
 	}
 	function renderUser(user) {
+		$username = user.username;
+		$password = user.password;
+		$firstName = user.firstName;
+		$lastName = user.lastName;
+		$role = user.role;
 	}
 	function renderUsers(users) {
 		for (var u = 0; u < users.length; u++) {
 			console.log(users[u]);
 			var clone = $userRowTemplate.clone();
 			clone.find(".username").html(users[u].username);
+			clone.find(".password").html(users[u].firstName);
 			clone.find(".firstName").html(users[u].firstName);
+			clone.find(".lastName").html(users[u].firstName);
+			clone.find(".role").html(users[u].firstName);
 			$tbody.append(clone);
 		}
 	}
--- webdev-sp19-seung-son-server-java-master/src/main/webapp/services/user.service.client.js
+++ webdev-sp19-seung-son-server-java-master/src/main/webapp/services/user.service.client.js
@@ -6,17 +6,33 @@ function AdminUserServiceClient() {
 	this.updateUser = updateUser;
 	this.url = 'http://localhost:8080/api/user';
 	var self = this;
-	function createUser(user, callback) {
+	function createUser(user) {
+		return fetch(url + '/' + userId, {
+		    method: 'PUT'
+		  })
+		  .then(response => response.json());
 	}
 	function findAllUsers() {
 		return fetch(this.url).then(function(response) {
 			return response.json();
 		});
 	}
-	function findUserById(userId, callback) {
+	function findUserById(userId) {
+		return fetch(this.url + "/" + userId).then(function(response) {
+			return response.json();
+		});
 	}
-	function updateUser(userId, user, callback) {
+	function updateUser(userId, user) {
+		return fetch(url + '/' + userId, {
+		    method: 'PUT',
+	    	body: JSON.stringify(user)
+		  })
+		  .then(response => response.json());
 	}
-	function deleteUser(userId, callback) {
+	function deleteUser(userId) {
+		return fetch(url + '/' + userId, {
+		    method: 'DELETE'
+		  })
+		  .then(response => response.json());
 	}
 }
