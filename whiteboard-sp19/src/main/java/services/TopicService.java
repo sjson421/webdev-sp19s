eec61a35034442c0cf0898e4d8dba8a6697ad754
Jae Son <sjson421@gmail.com>
2019-02-13 22:04:23 -0500

    topic service

--- whiteboard-sp19/src/main/java/services/TopicService.java
+++ whiteboard-sp19/src/main/java/services/TopicService.java
@@ -7,44 +7,14 @@ import org.springframework.web.bind.annotation.*;
 import models.Course;
 import models.Lesson;
 import models.Module;
+import models.Topic;
 
 @RestController
 public class TopicService {
 	List<Course> courses = CourseService.courses;
 
-	@PostMapping("/api/module/{mid}/lesson")
-	public Lesson createLesson(@PathVariable("mid") Integer id, @RequestBody Lesson lesson) {
-		for (int i = 0; i < courses.size(); i++) {
-			Course course = courses.get(i);
-			List<Module> modules = course.getModules();
-			for (int j = 0; j < modules.size(); j++) {
-				Module module = modules.get(j);
-				if (module.getId().equals(id)) {
-					module.getLessons().add(lesson);
-					return lesson;
-				}
-			}
-		}
-		return null;
-	}
-
-	@GetMapping("/api/module/{mid}/lesson")
-	public List<Lesson> findAllLessons(@PathVariable("mid") Integer id) {
-		for (int i = 0; i < courses.size(); i++) {
-			Course course = courses.get(i);
-			List<Module> modules = course.getModules();
-			for (int j = 0; j < modules.size(); j++) {
-				Module module = modules.get(j);
-				if (module.getId().equals(id)) {
-					return module.getLessons();
-				}
-			}
-		}
-		return null;
-	}
-
-	@GetMapping("/api/lesson/{lid}")
-	public Lesson findLessonById(@PathVariable("lid") Integer id) {
+	@PostMapping("/api/lesson/{lid}/topic")
+	public Topic createTopic(@PathVariable("lid") Integer id, @RequestBody Topic topic) {
 		for (int i = 0; i < courses.size(); i++) {
 			Course course = courses.get(i);
 			List<Module> modules = course.getModules();
@@ -54,7 +24,8 @@ public class TopicService {
 				for (int k = 0; k < lessons.size(); k++) {
 					Lesson lesson = lessons.get(k);
 					if (lesson.getId().equals(id)) {
-						return lesson;
+						lesson.getTopics().add(topic);
+						return topic;
 					}
 				}
 			}
@@ -62,8 +33,8 @@ public class TopicService {
 		return null;
 	}
 
-	@PutMapping("/api/lesson/{lid}")
-	public Lesson updateLesson(@PathVariable("lid") Integer id, @RequestBody Lesson newLesson) {
+	@GetMapping("/api/lesson/{lid}/topic")
+	public List<Topic> findAllTopics(@PathVariable("lid") Integer id) {
 		for (int i = 0; i < courses.size(); i++) {
 			Course course = courses.get(i);
 			List<Module> modules = course.getModules();
@@ -73,8 +44,7 @@ public class TopicService {
 				for (int k = 0; k < lessons.size(); k++) {
 					Lesson lesson = lessons.get(k);
 					if (lesson.getId().equals(id)) {
-						lessons.set(k, newLesson);
-						return newLesson;
+						return lesson.getTopics();
 					}
 				}
 			}
@@ -82,8 +52,8 @@ public class TopicService {
 		return null;
 	}
 
-	@DeleteMapping("/api/lesson/{lid}")
-	public void deleteLesson(@PathVariable("lid") Integer id) {
+	@GetMapping("/api/topic/{tid}")
+	public Topic findTopicById(@PathVariable("tid") Integer id) {
 		for (int i = 0; i < courses.size(); i++) {
 			Course course = courses.get(i);
 			List<Module> modules = course.getModules();
@@ -92,8 +62,59 @@ public class TopicService {
 				List<Lesson> lessons = module.getLessons();
 				for (int k = 0; k < lessons.size(); k++) {
 					Lesson lesson = lessons.get(k);
-					if (lesson.getId().equals(id)) {
-						lessons.remove(k);
+					List<Topic> topics = lesson.getTopics();
+					for (int l = 0; l < topics.size(); l++) {
+						Topic topic = topics.get(l);
+						if (topic.getId().equals(id)) {
+							return topic;
+						}
+					}
+				}
+			}
+		}
+		return null;
+	}
+
+	@PutMapping("/api/topic/{tid}")
+	public Topic updateTopic(@PathVariable("tid") Integer id, @RequestBody Lesson newLesson) {
+		for (int i = 0; i < courses.size(); i++) {
+			Course course = courses.get(i);
+			List<Module> modules = course.getModules();
+			for (int j = 0; j < modules.size(); j++) {
+				Module module = modules.get(j);
+				List<Lesson> lessons = module.getLessons();
+				for (int k = 0; k < lessons.size(); k++) {
+					Lesson lesson = lessons.get(k);
+					List<Topic> topics = lesson.getTopics();
+					for (int l = 0; l < topics.size(); l++) {
+						Topic topic = topics.get(l);
+						if (topic.getId().equals(id)) {
+							topics.set(l, topic);
+							return topic;
+						}
+					}
+				}
+			}
+		}
+		return null;
+	}
+
+	@DeleteMapping("/api/topic/{tid}")
+	public void deleteTopic(@PathVariable("tid") Integer id) {
+		for (int i = 0; i < courses.size(); i++) {
+			Course course = courses.get(i);
+			List<Module> modules = course.getModules();
+			for (int j = 0; j < modules.size(); j++) {
+				Module module = modules.get(j);
+				List<Lesson> lessons = module.getLessons();
+				for (int k = 0; k < lessons.size(); k++) {
+					Lesson lesson = lessons.get(k);
+					List<Topic> topics = lesson.getTopics();
+					for (int l = 0; l < topics.size(); l++) {
+						Topic topic = topics.get(l);
+						if (topic.getId().equals(id)) {
+							topics.remove(l);
+						}
 					}
 				}
 			}
