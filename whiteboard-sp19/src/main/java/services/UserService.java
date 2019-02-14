a8c04a644cdc4d8016fca1f8e12739f93cbb9bf9
Jae Son <sjson421@gmail.com>
2019-02-14 15:18:33 -0500

    user service done

--- whiteboard-sp19/src/main/java/services/UserService.java
+++ whiteboard-sp19/src/main/java/services/UserService.java
@@ -3,6 +3,9 @@ package services;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.web.bind.annotation.*;
 
 import models.Faculty;
@@ -13,30 +16,53 @@ import models.Student;
 public class UserService {
 	Faculty alice = new Faculty(123, "Alice", "Jondice", "alice", "alice", new Date(1990, 1, 3), "English");
 	Student bob = new Student(234, "Bob", "Fallon", "bob", "bob", new Date(1995, 2, 16), "Physics");
+	Student charlie = new Student(345, "Charlie", "Hood", "charlie", "charlie", new Date(1984, 6, 12), "Chemistry");
 
 	List<Person> users = new ArrayList<Person>();
 	{
 		users.add(alice);
 		users.add(bob);
+		users.add(charlie);
 	}
 
 	@PostMapping("/api/register")
-	public Person register(@RequestBody Person user) {
-		for (int i = 0; i < users.size(); i++) {
-			if (users.get(i).getUsername().equals(user.getUsername()))
-				return null;
-		}
+	public Person register(@RequestBody Person user, HttpSession session) {
+		session.setAttribute("currentUser", user);
 		users.add(user);
 		return user;
 	}
 
+	@GetMapping("/api/profile")
+	public Person profile(HttpSession session) {
+		Person currentUser = (Person) session.getAttribute("currentUser");
+		return currentUser;
+	}
+
+	@PostMapping("/api/login")
+	public Person login(@RequestBody Person credentials, HttpSession session) {
+		for (int i = 0; i < users.size(); i++) {
+			Person user = users.get(i);
+			if (user.getUsername().equals(credentials.getUsername())
+					&& user.getPassword().equals(credentials.getPassword())) {
+				session.setAttribute("currentUser", user);
+				return user;
+			}
+		}
+		return null;
+	}
+
+	@PostMapping("/api/logout")
+	public void logout(HttpSession session) {
+		session.invalidate();
+	}
+
 	@GetMapping("/api/users")
-	public List<Person> findAllCourses() {
+	public List<Person> findAllUsers() {
 		return users;
 	}
 
-	@GetMapping("/api/users/{userId}")
-	public Person findUserById(@PathVariable("userId") Integer id) {
+	@GetMapping("/api/users/{id}")
+	public Person findUserById(@PathVariable("id") Integer id) {
 		for (int i = 0; i < users.size(); i++) {
 			Person user = users.get(i);
 			if (user.getId().equals(id)) {
@@ -44,27 +70,5 @@ public class UserService {
 			}
 		}
 		return null;
-	}
-
-	@PutMapping("/api/users/{userId}")
-	public Person updateCourse(@PathVariable("userId") Integer id, @RequestBody Person newUser) {
-		for (int i = 0; i < users.size(); i++) {
-			Person user = users.get(i);
-			if (user.getId().equals(id)) {
-				users.set(i, newUser);
-				return newUser;
-			}
-		}
-		return null;
-	}
-
-	@DeleteMapping("/api/users/{userId}")
-	public void deleteCourse(@PathVariable("userId") Integer id) {
-		for (int i = 0; i < users.size(); i++) {
-			Person user = users.get(i);
-			if (user.getId().equals(id)) {
-				users.remove(user);
-			}
-		}
 	}
 }
