66b7d9dfad16f6090fc4f31aac12b58696187c06
Jae Son <sjson421@gmail.com>
2019-03-27 18:03:26 -0400

    assignment7

--- angular/server.js
+++ angular/server.js
@@ -5,11 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/assignment7-sp19s1-seung-son'));
+app.use(express.static('./dist/angular'));
 
 app.get('/*', function(req,res) {
-
-  res.sendFile(path.join(__dirname,'/dist/assignment7-sp19s1-seung-son'));
+  res.sendFile(path.join(__dirname,'/dist/angular/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
